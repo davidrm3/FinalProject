f7b62e011546fb807858cfe90e5fdf8467829c1e
David <118394042+davidrm3@users.noreply.github.com>
2022-12-08 01:00:22 -0600

    Levels files
    ssaf

---
+++ levels/Green1/Greenlvl1_rom.sv
@@ -0,0 +1,13 @@
+module Greenlvl1_rom (
+	input logic clock,
+	input logic [17:0] address,
+	output logic [3:0] q
+);
+
+logic [3:0] memory [0:195299] /* synthesis ram_init_file = "C:\Users\David\Desktop\ECE385\Final\Lab_6\levels\Green1\Greenlvl1.mif" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
---
+++ levels/Gutter2/Gutterslvl2_rom.sv
@@ -0,0 +1,13 @@
+module Gutterslvl2_rom (
+	input logic clock,
+	input logic [17:0] address,
+	output logic [3:0] q
+);
+
+logic [3:0] memory [0:195299] /* synthesis ram_init_file = "C:\Users\David\Desktop\ECE385\Final\Lab_6\levels\Gutter2\Gutterslvl2.mif" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
