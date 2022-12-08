398eb501a0107b31b33c618f8612cbbda23b0568
agsalud2 <118412528+agsalud2@users.noreply.github.com>
2022-12-08 16:03:31 -0600

    top and bottom screen collision

--- platforms.sv
+++ platforms.sv
@@ -5,14 +5,60 @@
 //((Char_Y_Pos + Char_Size) >= (Char_Y_Max)) //checks to see if char touches the bottom
 //((Char_Y_Pos - Char_Size) <= Char_Y_Min ) //checks to see if char touches the top
 
-module first_screen(input logic [9:0] Char_X_Pos, Char_Y_Pos, Char_Size,
-						  output top_collide, bottom_collide, right_collide, left_collide
+module touch_tb(input logic [9:0] Char_X_Pos, Char_Y_Pos, Char_Size, Char_X_Motion, Char_Y_Motion,
+					 output logic ts_collide, bs_collide
 );
 
-//bottom_platform
-always_comb //touching top edge of bottom platform
+always_comb
 	begin
-		if ((Char_Y_Pos + Char_Size) >= 407) 
+		if (Char_Y_Pos - Char_Size - 5) <= 5) //character touches top of the screen
+			begin
+				ts_collide = 1;
+			end
+		else
+			begin
+				ts_collide = 0;
+			end
+	end
+end
+
+always_comb
+	begin
+		if (Char_Y_Pos + Char_Size + 5) >= 470) //character touches bottom of the screen
+			begin
+				bs_collide = 1;
+			end
+		else
+			begin
+				bs_collide = 0;
+			end
+	end
+end
+
+endmodule
+
+
+
+module first_screen(input logic [9:0] Char_X_Pos, Char_Y_Pos, Char_Size, Char_X_Motion, Char_Y_Motion,
+						  output logic top_collide, bottom_collide, right_collide, left_collide
+);
+//any collision with a floor
+always_comb 
+	begin
+		if (((Char_Y_Pos + Char_Size + 5) >= 407) || //touching top edge of bottom platform
+		
+			(((Char_Y_Pos + Char_Size + 5) >= 240) && //character touches top edge of left platform
+			((Char_X_Pos + Char_Size) > 110) &&
+			((Char_X_Pos - Char_Size) < 210)) ||
+			
+			(((Char_Y_Pos + Char_Size + 5) >= 240) && //character touches top edge of right platform
+			((Char_X_Pos + Char_Size) > 432) && 
+			((Char_X_Pos - Char_Size) < 532)) ||
+			
+			(((Char_Y_Pos + Char_Size + 5) >= 80) && //character touches top edge of top platform
+			((Char_Y_Pos - Char_Size - 5) <= 130) &&
+			((Char_X_Pos + Char_Size) > 265) && 
+			((Char_X_Pos - Char_Size) < 378)))
 			begin
 				bottom_collide = 1;
 			end
@@ -22,12 +68,19 @@ always_comb //touching top edge of bottom platform
 			end
 	end
 
-//left_platform
-always_comb //character touches right edge of left platform
+//any collision with a right edge
+always_comb
 	begin
-		if (((Char_X_Pos - Char_Size) <= 210) && 
+		if (((Char_X_Pos - Char_Size -2) <= 210) && 
 			((Char_Y_Pos + Char_Size) > 240) && 
-			((Char_Y_Pos - Char_Size) < 407))
+			((Char_Y_Pos - Char_Size) < 407) ||
+			
+			((Char_X_Pos - Char_Size ) <= 110) ||
+			
+			((Char_X_Pos - Char_Size - 2) <= 378) && 
+			((Char_X_Pos + Char_Size - 2) >= 321) &&
+			((Char_Y_Pos + Char_Size) > 80) && 
+			((Char_Y_Pos - Char_Size) < 130))
 			 begin
 				left_collide = 1;
 			 end
@@ -36,26 +89,20 @@ always_comb //character touches right edge of left platform
 				left_collide = 0;
 			end
 	end
-always_comb
-	begin //character touches top edge of left platform
-		if (((Char_Y_Pos + Char_Size) >= 240) &&
-			((Char_X_Pos + Char_Size) > 110) &&
-			((Char_X_Pos - Char_Size) < 210))
-			begin
-				top_collide = 1;
-			end
-		else
-			begin
-				top_collide = 1;
-			end
-	end
 	
-//right_platform
-always_comb //character touches left edge of right platform
+//any collision with a left edge
+always_comb 
 	begin
-		if (((Char_X_Pos + Char_Size) >= 432) && 
+		if (((Char_X_Pos + Char_Size + 2) >= 432) && 
 			((Char_Y_Pos + Char_Size) > 240) && 
-			((Char_Y_Pos - Char_Size) < 407))
+			((Char_Y_Pos - Char_Size) < 407)||
+			
+			((Char_X_Pos + Char_Size) >= 533) ||
+			
+			((Char_X_Pos + Char_Size + 2) >= 265) && 
+			((Char_X_Pos - Char_Size + 2) <=320) &&
+			((Char_Y_Pos + Char_Size) > 80) && 
+			((Char_Y_Pos - Char_Size) < 130))
 			 begin
 				right_collide = 1;
 			 end
@@ -64,22 +111,7 @@ always_comb //character touches left edge of right platform
 				right_collide = 0;
 			end
 	end
-always_comb
-	begin //character touches top edge of right platform
-		if (((Char_Y_Pos + Char_Size) >= 240) &&
-			((Char_X_Pos + Char_Size) > 432) &&
-			((Char_X_Pos - Char_Size) < 533))
-			begin
-				top_collide = 1;
-			end
-		else
-			begin
-				top_collide = 1;
-			end
-	end
 
-//floating_platform
-	
 endmodule
 
 //module second_screen(
